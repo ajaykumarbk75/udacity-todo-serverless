0d8f0381602cdb1943132719ddff27844d4ecfdb
asaljic <amar.logins@outlook.com>
2021-02-08 09:13:26 +0100

    feat: Implemented GenerateUploadUrl function incl. serverless.yml modification
    GenerateUploadUrl function has been implemented incl. modification inside serverless.yml file to deploy the implemented function inside AWS Lambda

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,34 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler
+} from 'aws-lambda'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+import * as AWS from 'aws-sdk'
+
+const bucketName = process.env.ATTACHMENTS_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION_TIME
+
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+})
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  const signedUrl = s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: todoId,
+    Expires: urlExpiration
+  })
+
+  return {
+    statusCode: 201,
+    headers: {},
+    body: JSON.stringify({ uploadUrl: signedUrl })
+  }
 }
