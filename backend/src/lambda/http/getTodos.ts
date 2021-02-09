2d03f620760f447cb71eb89fdf70ee3e2dded20a
asaljic <amar.logins@outlook.com>
2021-02-09 09:37:36 +0100

    feat: Added logging to GetTodos function

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -7,13 +7,19 @@ import {
 } from 'aws-lambda'
 import { getUserId } from '../utils'
 import { getTodos } from '../../businessLogic/todos'
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('getTodos')
 
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   const userId = getUserId(event)
+
+  logger.info(`getTodos.ts: User ${userId} asks for access to their todos`)
   const items = await getTodos(userId)
 
+  logger.info(`getTodos.ts: Todos for User ${userId} have been loaded`)
   return {
     statusCode: 201,
     headers: {
