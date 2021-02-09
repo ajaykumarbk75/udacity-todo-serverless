f95b6a950c0a1b591da55ba4034c8f27f16c5346
asaljic <amar.logins@outlook.com>
2021-02-09 09:25:43 +0100

    feat: Added user permissions check and logging for generateUploadUrl
    generateUploadUrl now checks if the user has the rights to add an attachment to the specified todoId

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -9,9 +9,13 @@ import {
 import {
   getAttachmentLinkForTodo,
   getSignedUrl,
+  isUserAllowedToAccessTodo,
   updateAttachmentLinkOfTodo
 } from '../../businessLogic/todos'
 import { getUserId } from '../utils'
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('generateUploadUrl')
 
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
@@ -19,12 +23,38 @@ export const handler: APIGatewayProxyHandler = async (
   const todoId = event.pathParameters.todoId
   const userId = getUserId(event)
 
-  // 1. Get signed url
+  logger.info(
+    `generateUploadUrl.ts: user ${userId} wants to attach a file to todo ${todoId} and requests a signed url`
+  )
+
+  // 1. Check if user is allowed to access the selected todo - if not, return an adequate error message incl. status code
+  const isUserAllowed: Boolean = await isUserAllowedToAccessTodo(userId, todoId)
+  logger.info(
+    `generateUploadUrl.ts: User ${userId} is allowed to access todo ${todoId}: ${isUserAllowed}`
+  )
+  if (!isUserAllowed) {
+    logger.info(
+      `generateUploadUrl.ts: Signed URL for todo ${todoId} and user ${userId} has not been created because of insufficient permissions`
+    )
+    return {
+      statusCode: 403,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: ''
+    }
+  }
+
+  // 2. Get signed url
   const signedUrl: string = getSignedUrl(todoId)
 
-  // 2. Update todo item to contain attachmentUrl
+  // 3. Update todo item to contain attachmentUrl
   const attachmentUrl: string = getAttachmentLinkForTodo(todoId)
   await updateAttachmentLinkOfTodo(userId, todoId, attachmentUrl)
+  logger.info(
+    `generateUploadUrl.ts: Signed URL for todo ${todoId} and user ${userId} has been created: ${signedUrl}`
+  )
 
   return {
     statusCode: 201,
